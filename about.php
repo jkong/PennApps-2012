39aa54bc576f0d3e96ef07f07933a53b2474f847
Tommy <tpanfang@gmail.com>
2012-09-15 02:48:04 -0400

    updated hyperlinks and created about page

---
+++ about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta name="google-site-verification" content="B-rELLAyRf5kaXynTQldI32HBFNxVttzgksoT6AqWA8" />
+		<meta charset="utf-8">
+		<title>Clipmob</title>
+		<link rel="shortcut icon" type="image/png" href = "img/clipmobfavicon.png">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Find the best deals for you!">
+		<meta name="author" content="JJTN">
+		<!-- Le styles -->
+		<link href="stylesheets/bootstrap.css" rel="stylesheet">
+		<link href="stylesheets/bootstrap-responsive.css" rel="stylesheet">
+		<link rel="stylesheet/less" type="text/css" href="m.less">
+		
+		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<?php
+		include_once ('inc/navbar.inc.html');
+		?>		
+		<div class="container">
+			<div id="body">
+				<p> random text</p>
+			</div>
+		</div>
+		<?php
+		include_once ('inc/footer.inc.html');
+		?>
+		<!-- /container -->
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="http://code.jquery.com/jquery.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</body>
+</html>
