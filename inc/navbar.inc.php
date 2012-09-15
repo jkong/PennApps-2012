fc393f576082cfac975e0b0abddfe2f6a4b68d70
Joshua Stone <js2393@gmail.com>
2012-09-15 12:51:54 -0400

    Updated Facebook with PHP API

--- inc/navbar.inc.php
+++ inc/navbar.inc.php
@@ -1,3 +1,26 @@
+<?php
+
+define('YOUR_APP_ID', '479471488739276');
+
+//uses the PHP SDK.  Download from https://github.com/facebook/php-sdk
+require 'lib/fb-php-sdk/facebook.php';
+
+$facebook = new Facebook(array(
+  'appId'  => '479471488739276',
+  'secret' => '3fff44039c9aa1ff89155fd418443a4b',
+));
+
+$userId = $facebook->getUser();
+
+?>
+
+<div id="fb-root"></div>
+<?php if ($userId) { 
+      $userInfo = $facebook->api('/' . $userId); ?>
+      Welcome <?= $userInfo['name'] ?>
+    <?php } else { ?>
+    <fb:login-button></fb:login-button>
+    <?php } ?>
 <script src="js/fblogin.js" type="text/javascript"></script>
 <div id="wrapper">
   <div id="header">
