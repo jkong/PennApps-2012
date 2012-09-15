be5fe4dd8a77464cc9409410f57e9199cac8f835
Jason Kong <mw.jasonkong@gmail.com>
2012-09-14 22:02:26 -0400

    more files

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta name="google-site-verification" content="B-rELLAyRf5kaXynTQldI32HBFNxVttzgksoT6AqWA8" />
+		<meta charset="utf-8">
+		<title> PennApps 2012 </title>
+		<link rel="icon" type="image/png" href = "img/jobcoll.png">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Find the best deals for you!">
+		<meta name="author" content="JJTN">
+		<!-- Le styles -->
+		<link href="stylesheets/bootstrap.css" rel="stylesheet">
+		<link href="stylesheets/bootstrap-responsive.css" rel="stylesheet">
+		<link rel="stylesheet/less" type="text/css" href="m.less">
+		<script src="js/less.js" type="text/javascript"></script>
+		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<?php
+		include_once ('inc/navbar.inc.html');
+		?>
+		<div class="container">
+			<div id="body">
+				<!-- Main hero unit for a primary marketing message or call to action -->
+				<div class="hero-unit">
+					<div class="hero-unit clearfix">
+						<div class="pull-left span5">
+							<h1>Our Website</h1>
+							<p>
+								Description Here.
+							</p>
+							</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+		<?php
+		include_once ('inc/footer.inc.html');
+		?>
+		<!-- /container -->
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="http://code.jquery.com/jquery.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</body>
+</html>
