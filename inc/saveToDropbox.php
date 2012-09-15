cd61824a66a5658c70a67492907024cafb3d1d4a
Joshua Stone <js2393@gmail.com>
2012-09-15 16:32:19 -0400

    Dropbox

--- inc/saveToDropbox.php
+++ inc/saveToDropbox.php
@@ -1,7 +1,20 @@
 <?php
 
-function save_To_Dropbox($url) {
-  
-}
+//function save_To_Dropbox($url) {
+//}
+
+$consumerKey = 'z5zj4jmr5rou75m';
+$consumerSecret = 'yhm7fnbw760u06y';
+
+include '../lib/dropbox-php-sdk/autoload.php';
+
+session_start();
+$oauth = new Dropbox_OAuth_PHP($consumerKey, $consumerSecret);
+
+// If the PHP OAuth extension is not available, you can try
+// PEAR's HTTP_OAUTH instead.
+//$oauth = new Dropbox_OAuth_PEAR($consumerKey,$consumerSecret);
+
+$dropbox = new Dropbox_API($oauth);
 
 ?>
