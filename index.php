d86e1ba89ade0c36e40d035436c01f68d5fd155f
Jason Kong <mw.jasonkong@gmail.com>
2012-09-14 22:53:13 -0400

    facebook login

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta name="google-site-verification" content="B-rELLAyRf5kaXynTQldI32HBFNxVttzgksoT6AqWA8" />
 		<meta charset="utf-8">
 		<title> PennApps 2012 </title>
-		<link rel="icon" type="image/png" href = "img/jobcoll.png">
+		<link rel="icon" type="image/png" href = "img/fbicon.png">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="Find the best deals for you!">
 		<meta name="author" content="JJTN">
@@ -13,6 +13,7 @@
 		<link href="stylesheets/bootstrap-responsive.css" rel="stylesheet">
 		<link rel="stylesheet/less" type="text/css" href="m.less">
 		<script src="js/less.js" type="text/javascript"></script>
+		
 		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 		<!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -22,6 +23,7 @@
 		<?php
 		include_once ('inc/navbar.inc.html');
 		?>
+		
 		<div class="container">
 			<div id="body">
 				<!-- Main hero unit for a primary marketing message or call to action -->
@@ -30,7 +32,7 @@
 						<div class="pull-left span5">
 							<h1></h1>
 							<p>
-								Description Here.
+								 
 							</p>
 							</div>
 					</div>
